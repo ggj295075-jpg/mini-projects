7369e239f9626be1c60cb7bc8606d52320e0f6ae
ggj295075-jpg <ggj295075@gmail.com>
2026-07-29 15:44:46 +0400

    Added a new code for show, how work element-wise and matrix multiplication.

--- LinearAlgebra/NumpyLearnOutput.md
+++ LinearAlgebra/NumpyLearnOutput.md
@@ -4,3 +4,44 @@ Broadcast:
  [ 1  2  3  4]]
 Shape:
  (3, 4)
+Broadcast:
+ [[ 3  6  9 12]
+ [ 2  4  6  8]
+ [ 1  2  3  4]]
+Shape:
+ (3, 4)
+Element-wise multiplication(matrixA*matrixA):
+ [[ 1  4  9 16]
+ [25 36 49 64]]
+Matrix multiplication:
+ [[ 50  60]
+ [114 140]]
+Broadcast:
+ [[ 3  6  9 12]
+ [ 2  4  6  8]
+ [ 1  2  3  4]]
+Shape:
+ (3, 4)
+Element-wise multiplication(matrixA*matrixA):
+ [[ 1  4  9 16]
+ [25 36 49 64]]
+Matrix multiplication(mattrixA*matrixB):
+ [[ 50  60]
+ [114 140]]
+Matrix multiplication(matrixB*matrixA:
+ [[11 14 17 20]
+ [23 30 37 44]
+ [35 46 57 68]
+ [47 62 77 92]]
+VectorA:
+ [1 2 3 4]
+VectorB:
+ [3 2 1]
+MatrixA:
+ [[1 2 3 4]
+ [5 6 7 8]]
+MatrixB:
+ [[1 2]
+ [3 4]
+ [5 6]
+ [7 8]]
--- LinearAlgebra/numpyLearn.py
+++ LinearAlgebra/numpyLearn.py
@@ -7,3 +7,21 @@ Result = vectorA * vectorB[:, np.newaxis]
 print("Broadcast:\n", Result)
 print("Shape:\n", Result.shape)
 
+# Element-wise and matrix multiplication
+matrixA = np.array([[1,2,3,4],
+                   [5,6,7,8]])
+matrixB = np.array([[1,2],
+                   [3,4],
+                   [5,6],
+                   [7,8]])
+element_wise_result = matrixA * matrixA # or np.multiply(matrixA, matrixA)
+matrix_result = matrixA @ matrixB # or np.matmul(matrixA, matrixB)
+print("Element-wise multiplication(matrixA*matrixA):\n", element_wise_result)
+print("Matrix multiplication(mattrixA*matrixB):\n", matrix_result)
+print("Matrix multiplication(matrixB*matrixA:\n", matrixB @ matrixA)
+
+# Cut vectors and matrix:
+print("VectorA:\n",vectorA)
+print("VectorB:\n", vectorB)
+print("MatrixA:\n", matrixA)
+print("MatrixB:\n", matrixB)
